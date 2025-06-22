ea80a343fcdc88f4515c1c6dde3d264a2d85e2f5
Léu Almeida <leo@webid.net.br>
2025-06-22 12:41:16 -0300

    fix: fix logo location

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,13 @@ const plugins = [
     }
   },
   {
+    resolve: "gatsby-source-filesystem",
+    options: {
+      name: "images",
+      path: `${__dirname}/src/images/`
+    }
+  },
+  {
     resolve: 'gatsby-plugin-mdx',
     options: {
       gatsbyRemarkPlugins: [
