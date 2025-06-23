66ccec9c171319806feaad71b21f980c6dd60656
Léu Almeida <leo@webid.net.br>
2025-06-23 08:46:55 -0300

    fix: update node version to build

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,8 +35,7 @@ const plugins = [
         {
           resolve: "gatsby-remark-images",
           options: {
-            maxWidth: 1035,
-            sizeByPixelDensity: true
+            maxWidth: 1035
           }
         },
         {
